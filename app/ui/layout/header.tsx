e36259c00de1618c88642970baf7d36b3ea670e6
Alex Johnston <me@alexj.ca>
2025-03-04 12:58:30 -0700

    Work on responsiveness

--- app/ui/layout/header.tsx
+++ app/ui/layout/header.tsx
@@ -23,22 +23,24 @@ const navItems: { text: string, href: string }[] = [
 export default function Header() {
   return (
     <div className="flex justify-center shadow-md">
-      <div className="max-w-6xl w-full flex items-center">
+      <div className="max-w-6xl w-full flex flex-wrap items-center">
         <div className="py-4 pr-12">
           <Link href="/" className="text-6xl text-orange-vivid-500 italic font-bold">
             AHPC
           </Link>
         </div>
-        <ul className="flex gap-12 text-lg font-semibold grow">
-          {navItems.map(item => (
-            <li key={item.text}>
-              <Link href={item.href}>
-                {item.text}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <div className="flex items-center">
+        <div className="grow hidden md:inline">
+          <ul className="flex gap-12 text-lg font-semibold">
+            {navItems.map(item => (
+              <li key={item.text}>
+                <Link href={item.href}>
+                  {item.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex items-center justify-end grow">
           <div className="flex flex-col gap-1 pr-2">
             <div className="text-sm text-right">
               Alex Johnston
