f1b434b052020eed00e7887508d99ba89d982160
Alex Johnston <me@alexj.ca>
2025-03-04 07:37:25 -0700

    Work on upcoming events

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -11,7 +11,7 @@ const lateef = Lateef({
 
 export default function Home() {
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-wrap justify-center">
       <div className="max-w-6xl w-full">
         <h2 className={`text-5xl py-5 ${lateef.className}`}>Home</h2>
         <div className="flex bg-yellow-100 border-yellow-500 border-2 rounded-lg p-3 items-center gap-3">
@@ -22,6 +22,24 @@ export default function Home() {
             You have an oustanding invoice of <strong>$400.00</strong>.
           </div>
           <button className="bg-yellow-500 text-white p-2 rounded-sm">Pay Now</button>
+        </div>
+      </div>
+
+      <div className="max-w-6xl w-full border-2 border-orange-vivid-800 mt-5 rounded-xl">
+        <h2 className={`bg-orange-vivid-400 py-2 px-5 text-white font-bold text-3xl rounded-t-lg ${lateef.className}`}>Your Upcoming Events</h2>
+        <div>
+          <div className="py-2 px-5">
+            <h4 className="font-bold text-lg">Italian Style Pizza Bake off</h4>
+            <div>
+              April 23, 2025 | Edmonton
+            </div>
+          </div>
+          <div className="py-2 px-5">
+            <h4 className="font-bold text-lg">American Style Pizza Bake off</h4>
+            <div>
+              April 23, 2025 | Edmonton
+            </div>
+          </div>
         </div>
       </div>
     </div>
