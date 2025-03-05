bbae5484d0fcff030689d1d17b9ff166a878ebc5
Alex Johnston <me@alexj.ca>
2025-03-04 17:53:12 -0700

    Adaptive warning

--- app/ui/shared/warning-box.tsx
+++ app/ui/shared/warning-box.tsx
@@ -2,7 +2,7 @@ import { ExclamationCircleIcon } from "@heroicons/react/16/solid";
 import { ReactNode } from "react";
 
 export function WarningBox({ children } : { children: ReactNode }) {
-  return <div className="flex bg-yellow-100 border-yellow-500 border-2 rounded-lg p-3 items-center gap-3">
+  return <div className="flex flex-col sm:flex-row bg-yellow-100 border-yellow-500 border-2 rounded-lg p-3 items-center gap-3">
     <div className="w-10 text-yellow-500">
       <ExclamationCircleIcon />
     </div>
