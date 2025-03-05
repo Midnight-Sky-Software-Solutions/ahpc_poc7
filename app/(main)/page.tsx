1ad59b81dab205c00bd7ee22ea4aa0acaf2fa181
Alex Johnston <me@alexj.ca>
2025-03-04 17:57:50 -0700

    Adaptive about us

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -35,8 +35,8 @@ export default function Home() {
         </div>
 
         <div className="bg-orange-vivid-050 w-full mt-8 justify-center flex">
-          <div className="max-w-6xl grow flex ">
-            <div className="w-[50%] flex flex-col justify-center px-5">
+          <div className="max-w-6xl grow flex flex-wrap">
+            <div className="w-full md:w-[50%] flex flex-col justify-center px-5 h-78">
               <h3 className={`text-4xl pb-5 ${lateef.className}`}>About Us</h3>
               <p>
                 We are really passionate about pizza! The Association of Home Pizza Chefs brings together a community of pizza chefs who love their own brand.
@@ -45,8 +45,7 @@ export default function Home() {
                 Whether it’s Neapolitan, New York or Chicago Deep Dish, we have a place for you in our community.
               </p>
             </div>
-            <div className="bg-gray-200 w-[50%] h-78 bg-[url('/pizza-small.jpg')] bg-cover">
-              2
+            <div className="bg-gray-200 w-full md:w-[50%] h-78 bg-[url('/pizza-small.jpg')] bg-cover">
             </div>
           </div>
         </div>
